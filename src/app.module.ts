7da068b12086f34243f53ea7ebf32d91db88952e
Jacek Pilch <jaaahooou@wp.pl>
2021-10-13 16:41:46 +0200

    add basket

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PotatoShopController } from './potato-shop/potato-shop.controller';
+import { BasketController } from './basket/basket.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, PotatoShopController],
+  controllers: [AppController, PotatoShopController, BasketController],
   providers: [AppService],
 })
 export class AppModule {}
