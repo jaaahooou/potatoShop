5daf3cf46ce699275ef8ef4174a520d639daec21
Jacek Pilch <jaaahooou@wp.pl>
2021-10-18 20:12:34 +0200

    basic basket brontend

--- Frontend/js/menu.js
+++ Frontend/js/menu.js
@@ -2,3 +2,8 @@ const basketButton = document.querySelector('.basket-button');
 basketButton.addEventListener('click', () => {
   location.href = 'basket.html';
 });
+
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', () => {
+  location.href = 'index.html';
+});
