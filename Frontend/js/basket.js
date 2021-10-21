2f112a14bf420283ea2a0e57db680ae60e837fd6
Jacek Pilch <jaaahooou@wp.pl>
2021-10-21 21:03:45 +0200

    basket frontend

--- Frontend/js/basket.js
+++ Frontend/js/basket.js
@@ -42,6 +42,14 @@ function showBasketElement() {
         );
         productCountPriceWrapper.appendChild(productInBasketCountWrapper);
 
+        const productInBasketSubtrack = document.createElement('div');
+        productInBasketSubtrack.classList.add('product-in-basket-subtract');
+        productInBasketSubtrack.classList.add(
+          'product-in-basket-count-wrapper-element',
+        );
+        productInBasketSubtrack.innerText = `-`;
+        productInBasketCountWrapper.appendChild(productInBasketSubtrack);
+
         console.log(basketWrapper);
       }
     });
